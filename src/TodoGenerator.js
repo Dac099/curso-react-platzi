a5213318aa784d949982de5059c4641d956910c9
Dac099 <davidcejazapata@gmail.com>
2023-05-15 17:20:42 -0600

    estados

--- src/TodoGenerator.js
+++ src/TodoGenerator.js
@@ -1,4 +1,5 @@
 import "./TodoGenerator.css";
+import React, { useState } from "react";
 
 function getRandomPlaceholder(){
   const title_examples = [
@@ -18,23 +19,55 @@ function getRandomPlaceholder(){
 }
 
 function TodoGenerator() {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [color, setColor] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    console.log(title, category, color);
+  } 
+
   return (
     <article className="todo_generator">
       <p>Define tu nueva tarea</p>
-      <form className="todo_generator--form">
+      <form 
+        className="todo_generator--form"
+        onSubmit={e => handleSubmit(e)}
+      >
         <div>
           <label htmlFor="title">Titulo</label>
-          <input name="title" id="title" type="text" placeholder={getRandomPlaceholder()}></input>
+          <input 
+            name="title" 
+            id="title" 
+            type="text" 
+            placeholder={getRandomPlaceholder()}
+            // value={title}
+            onChange={e => setTitle(e.target.value)}
+          ></input>
         </div>
 
         <div>
           <label htmlFor="category">Categoria</label>
-          <input name="category" id="category" type="text"></input>
+          <input 
+            name="category" 
+            id="category" 
+            type="text"
+            // value={category}
+            onChange={e => setCategory(e.target.value)}
+          ></input>
         </div>
 
         <div>
           <label htmlFor="color">Color</label>
-          <input name="color" id="color" type="color"></input>
+          <input 
+            name="color" 
+            id="color" 
+            type="color"
+            // value={color}
+            onChange={e => setColor(e.target.value)}
+          ></input>
         </div>
 
         <button type="submit">Crear Tarea</button>
